0ffa59edb54088bf42bbfe19ad6b4c31fe1f3284
emmaculate-exceel <emmaculate2exceel@live.com>
2024-05-26 08:59:16 +0100

    mysql integration

--- back_end/app.py
+++ back_end/app.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
 #integration of files.
+""" connecting my frontend to my database """
+
+import MySQLdb
+
+#connection to the db
+db = MySQLdb.connect(host="100.25.129.60", port=3306, user="root", passwd="root", db="SILVERLINE_EXPRESS")
+
+#check if connected
+if db:
+    print("connected!!!")
+
+cursor = db.cursor()
+cursor.execute("SHOW DATABASES")
+database = cursor.fetchall()
+for rows in database:
+    print(rows)
+    
+cursor.close()
+db.close()
+
+
+if __name__ == "__main__":
+    main()
