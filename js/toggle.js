81fbd7de463b16c47e6506dff26b15beb70b3841
Arowoolo kehinde <arowolokehinde321@gmail.com>
2024-06-05 13:34:55 +0100

    updated the bokok now form to go to the signup page

--- js/toggle.js
+++ js/toggle.js
@@ -1,52 +1,52 @@
-const signInBtnEl = document.getElementById("sign-in");
-const formContainerEl = document.getElementById("login-form-container");
-// const overlay = document.getElementById("overlay");
-// const closeLogin = document.getElementsByClassName("close-login");
-const header = document.getElementById("header");
-const bookBtn = document.querySelectorAll(".book-now-btn");
+// const signInBtnEl = document.getElementById("sign-in");
+// const formContainerEl = document.getElementById("login-form-container");
+// // const overlay = document.getElementById("overlay");
+// // const closeLogin = document.getElementsByClassName("close-login");
+// const header = document.getElementById("header");
+// const bookBtn = document.querySelectorAll(".book-now-btn");
 
-const showSignForm = () => {
-  formContainerEl.classList.remove("hidden");
-  // overlay.classList.remove("hidden");
-  document.body.style.overflow = "hidden";
-};
+// const showSignForm = () => {
+//   formContainerEl.classList.remove("hidden");
+//   // overlay.classList.remove("hidden");
+//   document.body.style.overflow = "hidden";
+// };
 
-signInBtnEl.addEventListener("click", function () {
-  showSignForm();
-});
+// signInBtnEl.addEventListener("click", function () {
+//   showSignForm();
+// });
 
-bookBtn.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    e.preventDefault();
-    showSignForm();
-  });
-});
+// bookBtn.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     showSignForm();
+//   });
+// });
 
-formContainerEl.addEventListener("click", function (e) {
-  if (e.target.classList.contains("login-form-container")) {
-    formContainerEl.classList.add("hidden");
-    document.body.style.overflow = "scroll";
-  }
-});
+// formContainerEl.addEventListener("click", function (e) {
+//   if (e.target.classList.contains("login-form-container")) {
+//     formContainerEl.classList.add("hidden");
+//     document.body.style.overflow = "scroll";
+//   }
+// });
 
-document.addEventListener("DOMContentLoaded", function () {
-  var showSignupForm = document.getElementById("showSignupForm");
-  var showLoginForm = document.getElementById("showLoginForm");
-  var loginForm = document.getElementById("loginForm");
-  var signupForm = document.getElementById("signupForm");
+// document.addEventListener("DOMContentLoaded", function () {
+//   var showSignupForm = document.getElementById("showSignupForm");
+//   var showLoginForm = document.getElementById("showLoginForm");
+//   var loginForm = document.getElementById("loginForm");
+//   var signupForm = document.getElementById("signupForm");
 
-  showSignupForm.addEventListener("click", function (event) {
-    event.preventDefault();
-    loginForm.classList.add("hidden");
-    signupForm.classList.remove("hidden");
-  });
+//   showSignupForm.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     loginForm.classList.add("hidden");
+//     signupForm.classList.remove("hidden");
+//   });
 
-  showLoginForm.addEventListener("click", function (event) {
-    event.preventDefault();
-    signupForm.classList.add("hidden");
-    loginForm.classList.remove("hidden");
-  });
-});
+//   showLoginForm.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     signupForm.classList.add("hidden");
+//     loginForm.classList.remove("hidden");
+//   });
+// });
 
 // Get the moda
 var moda = document.getElementById("moda");
